ca8a0984c55ec56c70c13515d945a5e211f9d576
mythiliuk0601-Uk <mythiliuk0601@gmail.com>
2026-09-21 14:09:10 +0530

    Add home controller for root URL

---
+++ backend/src/main/java/com/campus/lostfound/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.campus.lostfound.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend is running successfully!";
+    }
+}
